11943007922b8b1f6861af9cbb4b58c321e499d0
yumy-nect-7 <tatsuya.kws.4282@gmail.com>
2026-04-28 15:30:08 +0900

    Add files via upload

--- practise_streamlit.py
+++ practise_streamlit.py
@@ -30,3 +30,15 @@ if st.button('Say hello'):
     st.write('Why hello there')
 else:
     st.write('goodbye')
+
+st.header('st.slider')
+st.subheader('Slider')
+age = st.slider('年齢?', 0, 130, 25)
+st.write("私は", age, "歳")
+
+st.subheader('Range slider')
+values = st.slider(
+    '値の範囲の指定',
+    0.0, 100.0, (25.0, 75.0)
+)
+st.write("値: ", values)
